580b771db668ddb0a2a2f6e4609d1c458585a66b
Taron <216916949+OneCrazyCoder@users.noreply.github.com>
2023-12-31 13:12:36 -0800

    Move command now works, including switching which key moves in mouselook mode

--- Source/InputMap.h
+++ Source/InputMap.h
@@ -31,6 +31,12 @@ Command commandForButtonAction(
 // Get command for executing given macro slot in given set
 Command commandForMacro(int theMacroSetID, u8 theMacroSlotID);
 
+// Get certain special-case keys directly
+u8 keyForMoveTurn(ECommandDir);
+u8 keyForMoveStrafe(ECommandDir);
+u8 keyForMouseLookMoveTurn(ECommandDir);
+u8 keyForMouseLookMoveStrafe(ECommandDir);
+
 // Get bitfield of EHudElements that should be shown
 BitArray8<eHUDElement_Num> visibleHUDElements(int theMode);
 
