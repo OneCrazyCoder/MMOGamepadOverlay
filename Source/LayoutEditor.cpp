aded78f74c126026b025a43f223f98f1168ab247
Taron <216916949+OneCrazyCoder@users.noreply.github.com>
2024-07-16 16:28:14 -0700

    More work on runtime UI layout editor - now lists items to potentially move

--- Source/LayoutEditor.cpp
+++ Source/LayoutEditor.cpp
@@ -6,6 +6,7 @@
 
 #include "Dialogs.h"
 #include "HotspotMap.h"
+#include "InputMap.h"
 #include "Profile.h"
 
 namespace LayoutEditor
@@ -17,6 +18,14 @@ namespace LayoutEditor
 //-----------------------------------------------------------------------------
 // Const Data
 //-----------------------------------------------------------------------------
+
+const char* kHotspotsPrefix = "Hotspots/";
+const char* kIconsPrefix = "Icons/";
+const char* kMenuPrefix = "Menu.";
+const char* kHUDPrefix = "HUD.";
+const char* kPositionKey = "Position";
+const char* kSizeKey = "Size";
+const char* kItemSizeKey = "ItemSize";
 
 
 //-----------------------------------------------------------------------------
@@ -33,20 +42,20 @@ struct LayoutEntry
 		eType_MenuCategory,
 		eType_HUDCategory,
 
-		eType_HotspotSingle,
-		eType_HotspotArray,
-		eType_HotspotRange,
-		eType_HotspotArrayElement,
+		eType_Hotspot,
 		eType_CopyIcon,
-		eType_CopyIconRange,
 		eType_Menu,
 		eType_HUD,
 
 		eType_Num,
-		eType_FirstMoveable = eType_HotspotSingle
+		eType_CategoryNum = eType_Hotspot
 	} type;
-	Hotspot value;
+	std::string keyName;
+	std::string sizeKeyName;
+	Hotspot position;
+	Hotspot size;
 	Dialogs::TreeViewDialogItem item;
+	u16 rangeCount;
 };
 
 
@@ -65,6 +74,120 @@ struct LayoutEntry
 #define layoutDebugPrint(...) ((void)0)
 #endif
 
+static void setEntryParent(
+	LayoutEntry& theEntry,
+	const StringToValueMap<u32>& theEntryNameMap)
+{
+	if( theEntry.rangeCount == 0 )
+		return;
+	std::string anArrayName = condense(theEntry.item.name);
+	const int anArrayEndIdx = breakOffIntegerSuffix(anArrayName);
+	DBG_ASSERT(anArrayEndIdx >= 0);
+	if( theEntry.rangeCount == 1 )
+	{// Search for an anchor hotspot to act as parent
+		const u32* aParentIdx = theEntryNameMap.find(anArrayName);
+		if( aParentIdx )
+			theEntry.item.parentIndex = *aParentIdx;
+		return;
+	}
+
+	// Search for previous hotspot in the hotspot array
+	DBG_ASSERT(anArrayName[anArrayName.size()-1] == '-');
+	anArrayName.resize(anArrayName.size()-1);
+	const int anArrayStartIdx = breakOffIntegerSuffix(anArrayName);
+	DBG_ASSERT(anArrayStartIdx >= 0);
+	const int anArrayPrevIdx = anArrayStartIdx - 1;
+	const u32* aParentIdx = theEntryNameMap.find(
+		anArrayName + toString(anArrayPrevIdx));
+	if( aParentIdx )
+	{
+		theEntry.item.parentIndex = *aParentIdx;
+		return;
+	}
+	StringToValueMap<u32>::IndexVector anIndexSet;
+	theEntryNameMap.findAllWithPrefix(anArrayName, &anIndexSet);
+	for(size_t i = 0; i < anIndexSet.size(); ++i)
+	{
+		std::string aHotspotName =
+			theEntryNameMap.keys()[anIndexSet[i]];
+		const int aHotspotEndIdx =
+			breakOffIntegerSuffix(aHotspotName);
+		if( aHotspotEndIdx == anArrayPrevIdx )
+		{
+			theEntry.item.parentIndex =
+				theEntryNameMap.values()[anIndexSet[i]];
+			return;
+		}
+	}
+	aParentIdx = theEntryNameMap.find(anArrayName);
+	if( aParentIdx )
+		theEntry.item.parentIndex = *aParentIdx;
+	return;
+}
+
+
+static void addArrayEntries(
+	std::vector<LayoutEntry>& theEntryList,
+	LayoutEntry::EType theCategoryType,
+	LayoutEntry::EType theEntryType)
+{
+	const size_t aFirstNewEntryIdx = theEntryList.size();
+	LayoutEntry aNewEntry;
+	aNewEntry.type = theEntryType;
+	aNewEntry.item.parentIndex = theCategoryType;
+	aNewEntry.item.isRootCategory = false;
+
+	Profile::KeyValuePairs aPropertySet;
+	Profile::getAllKeys(
+		theEntryList[theCategoryType].keyName,
+		aPropertySet);
+	StringToValueMap<u32> anEntryNameToIdxMap;
+	for(size_t i = 0; i < aPropertySet.size(); ++i)
+	{
+		aNewEntry.rangeCount = 0;
+		aNewEntry.item.name = aPropertySet[i].first;
+		aNewEntry.keyName = theEntryList[theCategoryType].keyName;
+		aNewEntry.keyName += aNewEntry.item.name;
+		std::string aKeyName = condense(aNewEntry.item.name);
+		anEntryNameToIdxMap.setValue(
+			aKeyName, u32(theEntryList.size()));
+		const int anArrayEndIdx = breakOffIntegerSuffix(aKeyName);
+		if( anArrayEndIdx > 0 )
+		{
+			aNewEntry.rangeCount = 1;
+			int anArrayStartIdx = anArrayEndIdx;
+			if( aKeyName[aKeyName.size()-1] == '-' )
+			{// Part of a range of values, like Name2-8
+				aKeyName.resize(aKeyName.size()-1);
+				anArrayStartIdx = breakOffIntegerSuffix(aKeyName);
+			}
+			aNewEntry.rangeCount += anArrayEndIdx - anArrayStartIdx;
+		}
+		std::string aHotspotDesc = aPropertySet[i].second;
+		HotspotMap::stringToHotspot(aHotspotDesc, aNewEntry.position);
+		HotspotMap::stringToHotspot(aHotspotDesc, aNewEntry.size);
+		theEntryList.push_back(aNewEntry);
+	}
+	aPropertySet.clear();
+
+	// Link items in arrays with their direct parents
+	for(size_t i = aFirstNewEntryIdx; i < theEntryList.size(); ++i)
+		setEntryParent(theEntryList[i], anEntryNameToIdxMap);
+}
+
+
+static std::pair<std::string, std::string> tryGetHUDHotspot(
+	const std::string& thePrefix,
+	const std::string& theElementName,
+	const char* thePropKey)
+{
+	std::pair<std::string, std::string> result;
+	result.first = thePrefix + theElementName + "/";
+	result.first += thePropKey;
+	result.second = Profile::getStr(result.first);
+	return result;
+}
+
 
 //-----------------------------------------------------------------------------
 // Global Functions
@@ -72,42 +195,105 @@ struct LayoutEntry
 
 void launch()
 {
-	logError("Layout editor functionality is not yet implemented!");
-	return;
-
 	// Gather information on elements that can be edited
 	layoutDebugPrint("Initializing layout editor\n");
+
+	// Start with all the categories
 	std::vector<LayoutEntry> aLayoutEntriesList;
-	LayoutEntry anEntry;
-	anEntry.type = LayoutEntry::eType_Root;
-	anEntry.item.parentIndex = 0;
-	anEntry.item.allowedAsResult = false;
-	aLayoutEntriesList.push_back(anEntry);
+	LayoutEntry aNewEntry;
+	aNewEntry.type = LayoutEntry::eType_Root;
+	aNewEntry.item.parentIndex = 0;
+	aNewEntry.item.isRootCategory = true;
+	aNewEntry.rangeCount = 0;
+	aLayoutEntriesList.push_back(aNewEntry);
+	aNewEntry.type = LayoutEntry::eType_HotspotCategory;
+	aNewEntry.item.name = "Hotspots";
+	aNewEntry.keyName = kHotspotsPrefix;
+	aLayoutEntriesList.push_back(aNewEntry);
+	aNewEntry.type = LayoutEntry::eType_CopyIconCategory;
+	aNewEntry.item.name = "Copy Icons";
+	aNewEntry.keyName = kIconsPrefix;
+	aLayoutEntriesList.push_back(aNewEntry);
+	aNewEntry.type = LayoutEntry::eType_MenuCategory;
+	aNewEntry.item.name = "Menus";
+	aNewEntry.keyName = kMenuPrefix;
+	aLayoutEntriesList.push_back(aNewEntry);
+	aNewEntry.type = LayoutEntry::eType_HUDCategory;
+	aNewEntry.item.name = "HUD Elements";
+	aNewEntry.keyName = kHUDPrefix;
+	aLayoutEntriesList.push_back(aNewEntry);
+	DBG_ASSERT(aLayoutEntriesList.size() == LayoutEntry::eType_CategoryNum);
+	for(size_t i = 0; i < aLayoutEntriesList.size(); ++i)
+		DBG_ASSERT(aLayoutEntriesList[i].type == i);
+	aNewEntry.item.isRootCategory = false;
 
-	anEntry.type = LayoutEntry::eType_HotspotCategory;
-	anEntry.item.name = "Hotspots";
-	aLayoutEntriesList.push_back(anEntry);
+	// Collect the hotspots
+	addArrayEntries(
+		aLayoutEntriesList,
+		LayoutEntry::eType_HotspotCategory,
+		LayoutEntry::eType_Hotspot);
 
-	// Gather all the elements that can be customized from current Profile
-	Profile::KeyValuePairs aPropertySet;
-	Profile::getAllKeys("Hotspots/", aPropertySet);
-	for(size_t i = 0; i < aPropertySet.size(); ++i)
+	// Collect the copy icon regions
+	addArrayEntries(
+		aLayoutEntriesList,
+		LayoutEntry::eType_CopyIconCategory,
+		LayoutEntry::eType_CopyIcon);
+
+	// Collect HUD Element / Menu position keys from InputMap
+	for(u16 i = 0; i < InputMap::hudElementCount(); ++i)
 	{
-		anEntry.type = LayoutEntry::eType_HotspotSingle;
-		anEntry.item.name = aPropertySet[i].first;
-		anEntry.item.parentIndex = 1;
-		anEntry.item.allowedAsResult =
-			anEntry.type >= LayoutEntry::eType_FirstMoveable;
-		std::string aHotspotDesc = aPropertySet[i].second;
-		HotspotMap::stringToHotspot(aHotspotDesc, anEntry.value);
-		aLayoutEntriesList.push_back(anEntry);
+		aNewEntry.rangeCount = 0;
+		aNewEntry.item.name = InputMap::hudElementKeyName(i);
+		if( aNewEntry.item.name.empty() )
+			continue;
+		std::pair<std::string, std::string> aHotspotStr;
+		for(LayoutEntry::EType aCatType = LayoutEntry::eType_MenuCategory,
+			anEntryType = LayoutEntry::eType_Menu;
+			aCatType < LayoutEntry::eType_CategoryNum;
+			aCatType = LayoutEntry::EType(aCatType+1),
+			anEntryType = LayoutEntry::EType(anEntryType+1))
+		{
+			const std::string& aPrefix = aLayoutEntriesList[aCatType].keyName;
+			aHotspotStr = tryGetHUDHotspot(
+				aPrefix, aNewEntry.item.name, kPositionKey);
+			if( !aHotspotStr.second.empty() )
+			{
+				aNewEntry.type = anEntryType;
+				aNewEntry.keyName = aHotspotStr.first;
+				aNewEntry.item.parentIndex = aCatType;
+				HotspotMap::stringToHotspot(
+					aHotspotStr.second, aNewEntry.position);
+				aHotspotStr = tryGetHUDHotspot(
+					aPrefix, aNewEntry.item.name, kSizeKey);
+				if( !aHotspotStr.second.empty() )
+				{
+					aNewEntry.sizeKeyName = aHotspotStr.first;
+					HotspotMap::stringToHotspot(
+						aHotspotStr.second, aNewEntry.size);
+				}
+				else
+				{
+					aHotspotStr = tryGetHUDHotspot(
+						aPrefix, aNewEntry.item.name, kItemSizeKey);
+					if( !aHotspotStr.second.empty() )
+					{
+						aNewEntry.sizeKeyName = aHotspotStr.first;
+						HotspotMap::stringToHotspot(
+							aHotspotStr.second, aNewEntry.size);
+					}
+				}
+				aLayoutEntriesList.push_back(aNewEntry);
+				break;
+			}
+		}
 	}
+
+	// Prepare for prompt dialog
 	if( aLayoutEntriesList.empty() )
 	{
 		logError("Current profile has no positional items to edit!");
 		return;
 	}
-
 	std::vector<Dialogs::TreeViewDialogItem*> aDialogList;
 	aDialogList.reserve(aLayoutEntriesList.size());
 	for(size_t i = 0; i < aLayoutEntriesList.size(); ++i)
@@ -117,6 +303,8 @@ void launch()
 	layoutDebugPrint("Returned selection value = %d (%s)\n",
 		anEntryToEdit,
 		aLayoutEntriesList[anEntryToEdit].item.name.c_str());
+	if( anEntryToEdit > 0 )
+		logNotice("Layout editor functionality not yet implemented!");
 }
 
 #undef LAYOUT_EDITOR_DEBUG_PRINT
