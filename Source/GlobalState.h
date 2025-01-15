8196bb874d1661f1fb68b6ec626f57eb5712fce5
Taron <216916949+OneCrazyCoder@users.noreply.github.com>
2025-01-15 14:15:12 -0800

    Various fixes and polish tweaks to recent new features

--- Source/GlobalState.h
+++ Source/GlobalState.h
@@ -45,6 +45,9 @@ extern BitVector<> gActiveHUD;
 // Which HUD elements are disabled (i.e. Menu w/o input assigned to control it)
 extern BitVector<> gDisabledHUD;
 
+// Flag that all HUD elements containing dynamic strings need redraw
+extern bool gRedrawDynamicHUDStrings;
+
 // Displays hotspots mouse cursor can jump to with Select Hotspot command
 extern EHotspotGuideMode gHotspotsGuideMode;
 
